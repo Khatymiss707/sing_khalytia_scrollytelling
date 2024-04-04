5f77f28413b3d547f4d4bdab751ff25528128d5c
Khatymiss <khalytia@hotmail.ca>
2024-04-04 17:26:49 -0400

    spritesheet_animation_scroll

--- script/script.js
+++ script/script.js
@@ -1,21 +1,22 @@
 
-//ANIMATION FLECHE
-gsap.from('.arrow', { 
-    repeat: -1,
-    yoyo: true,
-    y: '4%',
-  });
-
 //ANIMATION TIMER
-
 let scrollTimer; 
 
-window.addEventListener('scroll', function() {
-  document.body.classList.add('is-scrolling'); 
+window.addEventListener("scroll", function() {
+
+  document.body.classList.add('is-scrolling');
 
   clearTimeout(scrollTimer); 
-  
+
   scrollTimer = setTimeout(function() {
     document.body.classList.remove('is-scrolling'); 
-  }, 100); 
-}); 
+  }, 100);  
+  
+});
+
+//ANIMATION FLECHE
+gsap.from('.arrow', { 
+  repeat: -1,
+  yoyo: true,
+  y: '4%',
+});
